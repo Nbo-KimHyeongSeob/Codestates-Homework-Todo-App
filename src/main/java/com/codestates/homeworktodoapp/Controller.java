5575b184cfbad36cb773911a5374e5df56dcf80b
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2022-08-22 15:56:11 +0900

    Get Method api complete
    x

---
+++ src/main/java/com/codestates/homeworktodoapp/Controller.java
@@ -0,0 +1,16 @@
+package com.codestates.homeworktodoapp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/request")
+public class Controller {
+
+    @GetMapping
+    public String request(){
+        return "To-do Application !";
+    }
+}
